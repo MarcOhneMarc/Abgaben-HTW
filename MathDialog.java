7f1619040a272ff2d9fbed73bad6b79015a796f6
Marc Perwak <marcperwak@outlook.de>
2022-11-21 12:55:12 +0100

    Aufgabe 3 (Nullstellen berechnung) und Kommenting fertiggestellt

---
+++ MathDialog.java
@@ -0,0 +1,9 @@
+public class MathDialog {
+    private MathFunctions mathFunctions;
+
+    public static void main(String[] args){
+        System.out.println(MathFunctions.berechneNullstellen(2.5 , 1.5));
+        System.out.println(MathFunctions.berechneNullstellen(3 , 2.25));
+        System.out.println(MathFunctions.berechneNullstellen(1.5 , 2.5));
+    }
+}
