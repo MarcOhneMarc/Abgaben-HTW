8d02ba38965d09438439796047118f3300b60f4e
Marc <Marcperwak@outlook.de>
2022-11-24 08:56:55 +0100

    BlueJ Test Commit

--- MathDialog.java
+++ MathDialog.java
@@ -3,7 +3,7 @@ public class MathDialog {
 
     public static void main(String[] args){
         System.out.println(MathFunctions.berechneNullstellen(2.5 , 1.5));
-        System.out.println(MathFunctions.berechneNullstellen(3 , 2.25));
+        System.out.println(MathFunctions.berechneNullstellen(3 , 2.35));
         System.out.println(MathFunctions.berechneNullstellen(1.5 , 2.5));
     }
 }
