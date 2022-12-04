d275494373f931292a332f5c5174ebcc64300482
Jonas <jonasneu812@gmail.com>
2022-12-04 21:32:37 +0100

    Create __SHELL21.java

---
+++ Artikel - Lager/__SHELL21.java
@@ -0,0 +1,8 @@
+
+public class __SHELL21 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+LagerDialog.main(__bluej_param0);
+
+}}
