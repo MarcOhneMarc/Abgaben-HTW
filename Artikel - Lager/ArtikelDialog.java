9be9fe99ef5530d3155eb03d6ea62ef7a6b89a9e
Jonas <jonasneu812@gmail.com>
2022-12-04 17:01:59 +0100

    Update ArtikelDialog.java

--- Artikel - Lager/ArtikelDialog.java
+++ Artikel - Lager/ArtikelDialog.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * Bestandsfuehrung.
- * 
+ *
  * @author Marc Perwark & Jonas Neu
  * @06.11.2022
  */
@@ -10,7 +10,7 @@ public class ArtikelDialog {
     //initialisierung der Attribute
     private Artikel artikel;
     private Scanner input;
-    
+
     private static final int artikel_mit_bestand = 1;
     private static final int artikel_ohne_bestand = 2;
     private static final int zugang_eines_artikel = 3;
@@ -19,12 +19,12 @@ public class ArtikelDialog {
     private static final int art_aendern = 6;
     private static final int bestand_aendern = 7;
     private static final int ende = 0;
-    
+
     //Konstruktor zum initialisiern des Scanners
     public ArtikelDialog() {
         input = new Scanner(System.in);
     }
-    
+
     /**
      * Hauptschleife des Dialogprogramms.
      */
@@ -47,7 +47,7 @@ public class ArtikelDialog {
             }
         } while (funktion != ende);
     }
-    
+
     /**
      * Text des Hauptmenues
      */
@@ -61,10 +61,10 @@ public class ArtikelDialog {
         System.out.println("Press '7' zum ueberschreiben des bestands eines Artikels");
         System.out.println("Press '0' zum beenden des Programms");
     }
-    
+
     /**
-     * Einlesen der Funktion 
-     * 
+     * Einlesen der Funktion
+     *
      * @return Auswahl wird im Menu als ganzzahliger Wert zurückgegeben
      */
     private int einlesenFuktion() {
@@ -72,10 +72,10 @@ public class ArtikelDialog {
         input.nextLine();
         return funktion;
     }
-    
+
     /**
      * Abfrage welche Funktion ausgeführt werden soll
-     * 
+     *
      * @param funktion gibt die auszufuehrende Funktion als ganze Zahl an
      */
     private void ausfuehrenFunktion(int funktion) {
@@ -96,27 +96,27 @@ public class ArtikelDialog {
         } else if (funktion == ende) {
             System.out.println("Programmende!");
         } else {
-            System.out.println("Falsche Eingabe!");  
+            System.out.println("Falsche Eingabe!");
         }
     }
-    
+
      /**
      * Aufruf Konstruktor zum initialisiern eines Artikel mit Bestand
-     * 
+     *
      * @param artikelNr gibt die Artikelnummer als vierstellige ganzzahl an
      * @param art gibt als Zeichenkette an was der Artikel ist
      * @param bestand gibt den wahrenbestand als ganzzahl an
      */
     private void artikelmitbestand() {
         if (artikel != null) {
-            System.out.println("Der Artikel existiert schon!"); 
+            System.out.println("Der Artikel existiert schon!");
         } else {
-            System.out.println("Eingabe der ID als Ganzzahl mit 4 Ziffern:"); 
+            System.out.println("Eingabe der ID als Ganzzahl mit 4 Ziffern:");
             int artikelNr = input.nextInt();
             input.nextLine();
-            System.out.println("Eingabe der Art als String:"); 
+            System.out.println("Eingabe der Art als String:");
             String art = input.nextLine();
-            System.out.println("Eingabe des Bestandes:"); 
+            System.out.println("Eingabe des Bestandes:");
             int bestand = input.nextInt();
             System.out.println("Eingabe der Preis als Double:");
             double preis = input.nextDouble();
@@ -125,21 +125,21 @@ public class ArtikelDialog {
 
         }
     }
-    
+
     /**
      * Aufruf Konstruktor zum initialisiern eines Artikel ohne Bestand
-     * 
+     *
      * @param artikelNr gibt die Artikelnummer als vierstellige ganzzahl an
      * @param art gibt als Zeichenkette an was der Artikel ist
      */
     private void artikelOhneBestand() {
         if (artikel != null) {
-            System.out.println("Der Artikel existiert schon!"); 
+            System.out.println("Der Artikel existiert schon!");
         } else {
-            System.out.println("Eingabe der ID als Ganzzahl mit 4 Ziffern:"); 
+            System.out.println("Eingabe der ID als Ganzzahl mit 4 Ziffern:");
             int artikelNr = input.nextInt();
             input.nextLine();
-            System.out.println("Eingabe der Art als String:"); 
+            System.out.println("Eingabe der Art als String:");
             String art = input.nextLine();
             System.out.println("Eingabe der Preis als Double:");
             double preis = input.nextDouble();
@@ -150,14 +150,14 @@ public class ArtikelDialog {
 
     /**
      * Buchung einer Bestandserhoehung
-     * 
+     *
      * zugang gibt den zugang als ganzzahl an
      */
     private void bucheZugang() {
         if (artikel == null) {
-            System.out.println("Der Artikel existiert noch nicht!"); 
+            System.out.println("Der Artikel existiert noch nicht!");
         } else {
-            System.out.println("Gib zugang an:"); 
+            System.out.println("Gib zugang an:");
             int zugang = input.nextInt();
             input.nextLine();
             artikel.bucheZugang(zugang);
@@ -166,26 +166,26 @@ public class ArtikelDialog {
 
    /**
      * Buchung einer Bestandsverminderung
-     * 
+     *
      * abgang gibt den abgang als ganzzahl an
      */
     private void bucheAbgang() {
         if (artikel == null) {
-            System.out.println("Der Artikel existiert noch nicht!"); 
+            System.out.println("Der Artikel existiert noch nicht!");
         } else {
-            System.out.println("Gib abgang an:"); 
+            System.out.println("Gib abgang an:");
             int abgang = input.nextInt();
             input.nextLine();
             artikel.bucheAbgang(abgang);
         }
     }
-    
+
     /**
      * Ausgabe der Artikelattribute mit aktuellem Wert als Zeichenkette
      */
     private void tostring() {
         if (artikel == null) {
-            System.out.println("Der Artikel existiert noch nicht!"); 
+            System.out.println("Der Artikel existiert noch nicht!");
         } else {
             System.out.println(artikel.toString());
         }
@@ -193,35 +193,35 @@ public class ArtikelDialog {
 
     /**
      * set Art eines Artikels
-     * 
+     *
      * art gibt als Zeichenkette an was der Artikel ist
      */
     private void setArt() {
         if (artikel == null) {
-            System.out.println("Der Artikel existiert noch nicht!"); 
+            System.out.println("Der Artikel existiert noch nicht!");
         } else {
-            System.out.println("Geben sie eine Zeichenkette an, mit der sie die 'Art' überschreiben möchten:"); 
+            System.out.println("Geben sie eine Zeichenkette an, mit der sie die 'Art' überschreiben möchten:");
             String art = input.nextLine();
             artikel.setArt(art);
         }
     }
-    
+
     /**
      * set Benstand eines Artikels
-     * 
+     *
      * bestand gibt den wahrenbestand als ganzzahl an
      */
     private void setBestand() {
         if (artikel == null) {
-            System.out.println("Der Artikel existiert noch nicht!"); 
+            System.out.println("Der Artikel existiert noch nicht!");
         } else {
-            System.out.println("Geben sie eine Ganzzahl an, mit der sie den Bestand überschreiben möchten:"); 
+            System.out.println("Geben sie eine Ganzzahl an, mit der sie den Bestand überschreiben möchten:");
             int bestand = input.nextInt();
             input.nextLine();
             artikel.setBestand(bestand);
         }
     }
-    
+
     public static void main(String[] args) {
         new ArtikelDialog().start();
     }
