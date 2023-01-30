76d082a82c6af4fca552d9d3f4acf6e69ee92ea8
Jonas <jonasneu812@gmail.com>
2023-01-30 22:31:06 +0100

    qwe

--- LinesOfCode/LOCAuswertung.java
+++ LinesOfCode/LOCAuswertung.java
@@ -9,6 +9,12 @@ public class LOCAuswertung {
         evaluationOfFiles(args);
     }
     
+    /**
+     * Methode zum errechnen der Zeilen von den Dateien, deren Dateiname uebergeben wird.
+     * 
+     * @param files Dateinamen als Liste
+     * @throws NoFileGivenException Ausnahme, wenn keine Dateinamen uebergeben werden
+     */
     public static void evaluationOfFiles(String[] files) throws IOException {
         if(files.length <1) {
             throw new NoFileGivenException(NO_FILES_GIVEN);
@@ -28,6 +34,11 @@ public class LOCAuswertung {
         System.out.println("Gesammt: " + fileCount + " Dateien mit " + sumCount + " Zeilen Code");
     }
     
+    /**
+     * Hilfsmethode zum errechnen der Zeilen
+     * 
+     * @param arg Dateinamen als String
+     */
     private static int getLines(String arg) throws IOException {
         BufferedReader file = new BufferedReader(new FileReader(arg));
         int count = 0;
