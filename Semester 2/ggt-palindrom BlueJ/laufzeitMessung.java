538565311ed74d7b338ebe60640310135534ca0a
Marc <Marcperwak@outlook.de>
2023-04-24 21:21:46 +0200

    Ueb 14 gesamt

--- Semester 2/ggt-palindrom BlueJ/laufzeitMessung.java
+++ Semester 2/ggt-palindrom BlueJ/laufzeitMessung.java
@@ -7,16 +7,8 @@ public class laufzeitMessung {
         Palindrom palindromRe = new PalindromRekursiv();
         Palindrom palindromIt = new PalindromIterativ();
 
-
         BufferedReader var1 = new BufferedReader(new FileReader(fileName));
-<<<<<<< HEAD
-        //PrintWriter writer = new PrintWriter("messung.csv");
-
-        writer.println("The first line");
-        writer.println("The second line");
-=======
         FileWriter writer = new FileWriter("Semester 2/ggt-palindrom BlueJ/messung.csv", true);
->>>>>>> fa4dc0d41b40d8ce58c756149fee3b997207ea5a
         int var2 = 0;
         long sysTimeCur;
         long sysTimeAfter;
