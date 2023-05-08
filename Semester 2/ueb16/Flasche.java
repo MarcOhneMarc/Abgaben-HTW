a208ca8f24bc24459f40567f12a236011471c144
Jonas <jonasneu812@gmail.com>
2023-05-08 17:46:12 +0200

    we

--- Semester 2/ueb16/Flasche.java
+++ Semester 2/ueb16/Flasche.java
@@ -7,19 +7,31 @@
  */
 public class Flasche <T extends Getraenk>{
     private T inhalt;
-    
+
+    /**
+     * Diese Methode fuellt eine Flassche mit einem Inhalt 
+     * aus dem Erbbaum Getraenk
+     * 
+     * @param object Ein Objekt aus dem Erbbaum Getraenk
+     */
     public void fuellen(T object) {
         this.inhalt = object;
     }
-    
-    public void leeren(T object) {
+
+    /**
+     * Diese Methode leert eine Flassche
+     * 
+     * @param object Ein Objekt aus dem Erbbaum Getraenk
+     */
+    public void leeren() {
         this.inhalt = null;
     }
     
+    /**
+     * Diese Methode gibt den Inhalt einer Flasche zurueck.
+     */
     public T getInhalt() {
         return inhalt;
     }
-    
-    //TOSTRING ??????????????????????????????????
     
 }
