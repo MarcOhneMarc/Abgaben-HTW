cb20af26edc71ce535d381d5647497083eafd13d
Marc <Marcperwak@outlook.de>
2023-06-27 00:46:02 +0200

    ueb19 sync

--- Semester 2/ueb20/aufgabe1/Main.java
+++ Semester 2/ueb20/aufgabe1/Main.java
@@ -1,38 +1,38 @@
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Random;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
+        String argumets = Arrays.toString(args);
+
+        System.out.println(",");
+
         Collection<Integer> collection;
-        if (args == "fifo") {
+        if (argumets.equals("fifo") || argumets.equals("FIFO")) {
             collection = new LinkedList<>();
-        } else {
+        } else  {
             collection = new PriorityQueue<>();
         }
+
+        System.out.println(",");
 
         Producer producer = new Producer(collection);
         Consumer consumer = new Consumer(collection);
 
-        Random random = new Random();
-        for (int i = 0; i < 10000; i++) {
-            if (random.nextInt(2) > 0) {
-                Thread producerThread = new Thread(producer);
-                producerThread.start();
-            } else {
-                Thread consumerThread = new Thread(consumer);
-                consumerThread.start();
-            }
-        }
+        System.out.println(",");
+        Thread producerThread = new Thread(producer);
+        producerThread.start();
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
 
+
+        System.out.println(",");
         try {
             Thread.sleep(1000); // Warten bis alle threds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        System.out.println(",");
         System.out.println("Number of different results: " + consumer.numberOfDifferentResults());
         //System.out.println("Occurrences of 123: " + consumer.numberOfOccurrences(123));
         System.out.println("Cross totals (ascending): " + consumer.getCrossTotalsAscending());
