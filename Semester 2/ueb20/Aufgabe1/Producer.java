e34c196093f1565a807ae45989c741eda7e684db
Jonas <jonasneu812@gmail.com>
2023-06-27 01:44:21 +0200

    qwe

--- Semester 2/ueb20/Aufgabe1/Producer.java
+++ Semester 2/ueb20/Aufgabe1/Producer.java
@@ -1,14 +1,18 @@
 import java.util.Random;
 
 /**
- * Beschreiben Sie hier die Klasse Producer.
+ * Producer
  *
- * @author (Ihr Name)
- * @version (eine Versionsnummer oder ein Datum)
+ * @author JonasNeu_MarcPerwak
  */
 class Producer {
+    /**
+     * Erstellt Zufallszahl zwischen 1 und 1000.
+     * 
+     * @return Die Zufallszahl
+     */
     public int produce() {
         Random random = new Random();
-        return random.nextInt(1001);
+        return random.nextInt(1, 1000);
     }
 }
